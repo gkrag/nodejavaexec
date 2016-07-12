1648da761ae7aa72c1507052b11246a6bb7ae6af
Girish Kumar Raghupati (Tata Consultancy Services) <v-giragh@microsoft.com>
2016-07-12 15:57:29 -0700

    Updated port to be dynamic

--- app.js
+++ app.js
@@ -23,7 +23,8 @@ function getJavaVersion(res){
     });
 }
 
-var server = app.listen(80, function () {
+
+var server = app.listen(process.env.PORT || 3000, function () {
   var host = server.address().address
   var port = server.address().port
 
