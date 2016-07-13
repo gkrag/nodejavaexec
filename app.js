a37b7bcffd5db5bfeb08d68945b2a0a62ec8e4ef
Girish Kumar Raghupati (Tata Consultancy Services) <v-giragh@microsoft.com>
2016-07-13 16:01:36 -0700

    Added invoke and commit api end points

--- app.js
+++ app.js
@@ -1,14 +1,74 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 var fs = require("fs");
-var AZURE_JDK_PATH = 'D:\\Program Files\\Java\\jdk1.8.0_25\\bin'
+var AZURE_JDK_PATH = 'D:\\Program Files\\Java\\jdk1.8.0_25\\bin';
+var CONVERTER_LIST = "\\pipeline\\_scr_desc_";
+var jobConfig = null;
 
 // initialize JDK path
 process.env.PATH += ';' + AZURE_JDK_PATH + ';';
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
 app.get('/ping', function (req, res) {
     console.log('Ping received.');
     getJavaVersion(res);
+})
+
+app.get('/config', function(req, res){
+    var xml2js = require('xml2js');
+    var parser = new xml2js.Parser();
+    
+    fs.readFile(__dirname + CONVERTER_LIST, function(err, data) {
+        parser.parseString(data, function (err, result) {
+            jobConfig = buildConfigJSON(result);
+            res.writeHead(200, {"Content-Type": "application/json"});
+            res.end(JSON.stringify(jobConfig));
+        });
+    });
+});
+
+function buildConfigJSON(config){
+    var configJSON = {};
+    
+    if(config){
+        for(var i = 0; i < config.scripts.script.length; i++){
+            var configNode = config.scripts.script[i];
+            var type = configNode.$.type;
+            configJSON[configNode.$.name] = {}
+            configJSON[configNode.$.name]['type'] = type;
+            if(type = 'simple')
+            {
+                if(typeof(configNode.input) != 'undefined' && typeof(configNode.ext) != 'undefined')
+                    configJSON[configNode.$.name]['input'] = {'type' : configNode.input[0], 'extension' : configNode.ext[0]};
+                    
+                if(typeof(configNode.command) != 'undefined')
+                    configJSON[configNode.$.name]['command'] = configNode.command[0];
+                    
+                if(typeof(configNode.output) != 'undefined' && typeof(configNode.type) != 'undefined' && typeof(configNode.extension) != 'undefined')
+                    configJSON[configNode.$.name]['output'] = {'type' : configNode.output[0], 'filetype': configNode.type[0], 'extension': configNode.extension[0]}; 
+            }
+            else if(type = 'complex')
+            {
+                
+            }
+        }
+    }
+    return configJSON;
+}
+
+app.post('/invoke', function (req, res) {
+    console.log('Invoked new job conversion.');
+    var postMessage = req.body;
+    
+    
+    
+    
+    
+    res.writeHead(200, {"Content-Type": "application/json"});
+    res.end(JSON.stringify({'status':'ok'}));
 })
 
 function getJavaVersion(res){
